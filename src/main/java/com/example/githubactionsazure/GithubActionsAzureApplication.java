6810e8a10a86c8d69e6db72b5f2f7a994f306849
christianmanuTemper <christian.manu@temperpack.com>
2022-09-02 16:00:06 -0400

    initial commit

---
+++ src/main/java/com/example/githubactionsazure/GithubActionsAzureApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubactionsazure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsAzureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionsAzureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githubactionsazure/GithubActionsAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubactionsazure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsAzureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
